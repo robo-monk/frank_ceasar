31820f0436a3da304b66a6f8c3fde86460fd155a
robo-monk <filtap007@gmail.com>
2020-02-06 18:46:03 +0100

    final frank ceasar commit

---
+++ ccr.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  txt = params['txt']
+  shft = params['shft']
+  if txt.nil? || shft.nil?
+    out = "-"
+  else
+    out = caesar_cipher(txt, shft)
+  end
+  erb :index, :locals => {:output => out}
+end
+
+def caesar_cipher (str, shift)
+  upcases = ('A'..'Z').to_a
+  downcases = ('a'..'z').to_a
+  shift = shift.to_i
+  encrypted = ''
+  str.chars.map do |char|
+    if(upcases.include?(char))
+        encrypted += (((char.ord + shift - 65) % 26 ) + 65).chr
+    elsif(downcases.include?(char))
+        encrypted += (((char.ord + shift - 97) % 26) + 97).chr
+    else
+        encrypted += char
+    end
+  end 
+  encrypted
+end
